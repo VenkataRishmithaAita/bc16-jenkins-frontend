11e2b46b42bae70639182d671aeb4af8c68e06f2
VenkataRishmithaAita <54346222+VenkataRishmithaAita@users.noreply.github.com>
2021-10-05 22:05:16 +0530

    Update JobService.js

--- src/service/JobService.js
+++ src/service/JobService.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 export const jobResource = `http://18.223.182.178:30897/api/v1/jobs`;
 export const base=window.location.origin;
-=======
 export const jobResource = `http://localhost:8081/api/v1/jobs`;
->>>>>>> bae76f5a195d7d20a45f28aff31444671e9e2329
 // const jobResource = `http://localhost:9000/jobs`;
 import axios from "axios";
 
